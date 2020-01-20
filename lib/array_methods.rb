4be1f329a7d29e51b8c1c296874dce54763b096e
Placido Wang <placidow@gmail.com>
2020-01-20 09:06:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  # array.index(value_to_find)
+  
+  array.length.times { |i|
+    if array[i] === value_to_find return i
+  }
 end
 
 def find_max_value(array)
