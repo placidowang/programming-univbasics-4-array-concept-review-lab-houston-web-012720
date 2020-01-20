785ff877c78b24699f8668e62ed6bc6636196514
Placido Wang <placidow@gmail.com>
2020-01-20 09:06:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   # array.index(value_to_find)
   
   array.length.times { |i|
-    if array[i] === value_to_find return i
+    if (array[i] === value_to_find) return i
   }
 end
 
